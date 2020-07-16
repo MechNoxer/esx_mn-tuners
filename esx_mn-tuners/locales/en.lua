0c3fbe5e3e9e570d67fd067507c80b15819a46d7
MechNoxer <osmauricedelauw@gmail.com>
2020-07-16 20:05:12 +0200

    Update en.lua

--- esx_mn-tuners/locales/en.lua
+++ esx_mn-tuners/locales/en.lua
@@ -1,3 +1,11 @@
 Locales['en'] = {
+    ['PRESS'] = "Press ~INPUT_CONTEXT~ to open tuning menu",
+    ['not'] = "Car is not tunable",
+    ['not_owner'] = "You are not the owner of the car!",
 
+    ['tune'] = "Tune",
+    ['show_car'] = "Show car",
+    ['remove_car'] = "Remove car",
+    ['back'] = "Back",
+    ['close'] = "Close",
 }
