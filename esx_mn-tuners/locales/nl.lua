5d3e9b69af0ff07f1f992be7135fbe20eadcbef2
MechNoxer <osmauricedelauw@gmail.com>
2020-07-16 20:05:28 +0200

    Update nl.lua

--- esx_mn-tuners/locales/nl.lua
+++ esx_mn-tuners/locales/nl.lua
@@ -1,3 +1,11 @@
 Locales['nl'] = {
+    ['PRESS'] = "Druk ~INPUT_CONTEXT~ om het tuning menu te openen",
+    ['not'] = "Auto kan niet getuned worden",
+    ['not_owner'] = "Je bent niet de eigenaar van het voortuig!",
 
+    ['tune'] = "Tune",
+    ['show_car'] = "Toon auto",
+    ['remove_car'] = "Verwijder auto",
+    ['back'] = "Terug",
+    ['close'] = "Sluit",
 }
